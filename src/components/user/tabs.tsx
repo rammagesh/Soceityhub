02923ad90a128d008d3a71c6bf7677ad55280312
Ramkumar M <117254689+RamkumarManokaran@users.noreply.github.com>
2025-05-29 19:52:58 +0530

    fixes for iphone tab spacing

--- src/components/user/tabs.tsx
+++ src/components/user/tabs.tsx
@@ -41,7 +41,7 @@ const Tabs = () => {
     </Link>
   ));
   return (
-    <div className="bg-default fixed left-[0] bottom-[0] w-full border-t-[2px] border-b-gray-500 flex justify-around py-[8px] items-center">
+    <div className="bg-default ios-pb-20 fixed left-[0] bottom-[0] w-full border-t-[2px] border-b-gray-500 flex justify-around py-[8px] items-center">
       {tabslist}
     </div>
   );
